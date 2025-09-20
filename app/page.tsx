dd24f66bc0712fd376ee5bcb48140db5be6b1fa3
ITM16141 <i.atsuki611@gmail.com>
2025-09-20 18:58:09 +0900

    いらない子を削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { PraiseForm } from "@/components/praise-form"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
   return (
